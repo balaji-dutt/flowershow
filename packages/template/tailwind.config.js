6e5394b9c489f3acaf0d32591dfd76103510dcd0
khalilcodes <khalilali.j@gmail.com>
2022-12-14 22:07:49 +0400

    [styles,tailwind][s]: add default fonts and custom colors

--- packages/template/tailwind.config.js
+++ packages/template/tailwind.config.js
@@ -1,3 +1,6 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+const colors = require("tailwindcss/colors");
+
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
@@ -11,6 +14,26 @@ module.exports = {
       // support wider width for large screens >1440px eg. in hero
       maxWidth: {
         "8xl": "88rem",
+      },
+      fontFamily: {
+        sans: ["ui-sans-serif", ...defaultTheme.fontFamily.sans],
+        serif: ["ui-serif", ...defaultTheme.fontFamily.serif],
+        mono: ["ui-monospace", ...defaultTheme.fontFamily.mono],
+        headings: ["-apple-system", ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        background: {
+          DEFAULT: colors.slate[50],
+          dark: colors.slate[900],
+        },
+        primary: {
+          DEFAULT: colors.slate[900],
+          dark: colors.slate[50],
+        },
+        secondary: {
+          DEFAULT: "",
+          dark: "",
+        },
       },
     },
   },
